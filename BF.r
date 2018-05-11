8759799c2308e49b1ff8641e50cba489695b66a6
dunkel13 <fedom9@gmail.com>
2018-05-10 23:25:04 -0500

    Update BF.r

--- BF.r
+++ BF.r
@@ -23,9 +23,15 @@ z=c(0,0,1,4,5)
 BF1=round(BF(A,C,n,x),4)
 BF2=round(BF(A,C,n,z),4)
   
-B=c(4,4)
+B=c(4,30)
 D=c(4,2)
 BF3=round(BF(B,D,n,x),4)
 BF4=round(BF(B,D,n,z),4)
-t2<-data.frame(c1=c(BF1,BF2), c2=c(BF3, BF4), row.names=c("(0,1,2,3,4)", "(0,0,1,4,5)"));t2
+
+BF5=round(BF(B,C,n,x),4)
+BF6=round(BF(B,C,n,z),4)
+
+BF7=round(BF(A,D,n,x),4)
+BF8=round(BF(A,D,n,z),4)
+t2<-data.frame(c1=c(BF1,BF2), c2=c(BF3, BF4),c3=c(BF5,BF6), c4=c(BF7, BF8), row.names=c("(0,1,2,3,4)", "(0,0,1,4,5)"));t2
 rm(list=ls())
